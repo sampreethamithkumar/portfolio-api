d82cb6a2a40132c3a6822f638f3a4740341f8245
sami0013 <sami0013@student.monash.edu>
2021-01-10 14:04:05 +1100

    adding cors lib

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
 const email = require("./routes/contact");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
 
 // Middleware
 app.use("/api/email", email);
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
+app.use(cors());
 
 const port = process.env.PORT || 3900;
 app.listen(port, () => {
